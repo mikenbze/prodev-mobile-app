2a9a24aaaf21be1049076418a2fe17637f15161e
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-09-09 07:38:55 +0300

    Update app 04

---
+++ prodev-mobile-app-0x05/constants/data.ts
@@ -0,0 +1,34 @@
+// constants/data.ts
+
+export const FILTERS = [
+  "Mansion",
+  "Apartment",
+  "Villa",
+  "Cottage",
+  "Studio",
+  "Penthouse",
+];
+
+export const SAMPLE_DATA = [
+  {
+    id: "1",
+    title: "Modern Beachfront Villa",
+    location: "Diani, Kenya",
+    price: "$200/night",
+    image: require("@/assets/images/villa.png"),
+  },
+  {
+    id: "2",
+    title: "Luxury City Apartment",
+    location: "Nairobi, Kenya",
+    price: "$150/night",
+    image: require("@/assets/images/apartment.png"),
+  },
+  {
+    id: "3",
+    title: "Cozy Countryside Cottage",
+    location: "Naivasha, Kenya",
+    price: "$80/night",
+    image: require("@/assets/images/cottage.png"),
+  },
+];
